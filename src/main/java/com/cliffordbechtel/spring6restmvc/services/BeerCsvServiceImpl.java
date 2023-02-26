3d6e8e2bc85b4f4d8a1c3e9e358675e4a33c3ca0
Clifford <crbechtel@gmail.com>
2023-02-25 19:11:23 -0500

    save CSV data to DB

--- src/main/java/com/cliffordbechtel/spring6restmvc/services/BeerCsvServiceImpl.java
+++ src/main/java/com/cliffordbechtel/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -2,12 +2,14 @@ package com.cliffordbechtel.spring6restmvc.services;
 
 import com.cliffordbechtel.spring6restmvc.model.BeerCSVRecord;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
     public List<BeerCSVRecord> convertCSV(File csvFile) {
