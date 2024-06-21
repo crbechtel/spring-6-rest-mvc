2a6130ce5c8000b31c2b57b57211ab4b25f70923
cliffordbechtel <crbechtel@gmail.com>
2024-06-21 19:40:21 -0400

    5 - Project Lombok

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,9 @@
+
+package guru.springframework.spring6restmvc.model;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
